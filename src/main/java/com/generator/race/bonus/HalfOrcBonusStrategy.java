a0ede76460499313e869b073b15bfc5491457b09
Francesco Boscaino <f.boscaino@gmail.com>
2024-01-03 23:39:08 +0100

    Add racial bonuses

--- src/main/java/com/generator/race/bonus/HalfOrcBonusStrategy.java
+++ src/main/java/com/generator/race/bonus/HalfOrcBonusStrategy.java
@@ -1,8 +1,10 @@
 package com.generator.race.bonus;
 
+import com.generator.CharacterBonus;
 import com.generator.Language;
 import com.generator.Skill;
 
+import java.util.List;
 import java.util.Set;
 
 public class HalfOrcBonusStrategy extends BonusStrategy {
@@ -23,6 +25,25 @@ public class HalfOrcBonusStrategy extends BonusStrategy {
 
     public Set<Skill> getProficiencies() {
         return Set.of(Skill.INTIMIDATION);
+    }
+
+    @Override
+    public List<CharacterBonus> getOtherBonus() {
+        List<CharacterBonus> otherBonus = super.getOtherBonus();
+        otherBonus.add(new CharacterBonus(
+                "Darkvision",
+                "Thanks to your orc blood, you have superior vision in dark and dim conditions. You can " +
+                        "see in dim light within 60 feet of you as if it were bright light, and in darkness as if it " +
+                        "were dim light. You can’t discern color in darkness, only shades of gray."));
+        otherBonus.add(new CharacterBonus(
+                "Relentless Endurance",
+                "When you are reduced to 0 hit points but not killed outright, you can drop to 1 hit point " +
+                        "instead. You can’t use this feature again until you finish a long rest."));
+        otherBonus.add(new CharacterBonus(
+                "Savage Attacks",
+                "When you score a critical hit with a melee weapon attack, you can roll one of the weapon’s " +
+                        "damage dice one additional time and add it to the extra damage of the critical hit."));
+        return otherBonus;
     }
 
 }
--- src/main/java/com/generator/race/bonus/TieflingBonusStrategy.java
+++ src/main/java/com/generator/race/bonus/TieflingBonusStrategy.java
@@ -1,7 +1,9 @@
 package com.generator.race.bonus;
 
+import com.generator.CharacterBonus;
 import com.generator.Language;
 
+import java.util.List;
 import java.util.Set;
 
 public class TieflingBonusStrategy extends BonusStrategy {
@@ -19,6 +21,27 @@ public class TieflingBonusStrategy extends BonusStrategy {
         Set<Language> languages = super.getLanguages();
         languages.add(Language.INFERNAL);
         return languages;
+    }
+
+    @Override
+    public List<CharacterBonus> getOtherBonus() {
+        List<CharacterBonus> otherBonus = super.getOtherBonus();
+        otherBonus.add(new CharacterBonus(
+                "Darkvision",
+                "Thanks to your infernal heritage, you have superior vision in dark and dim conditions. You " +
+                        "can see in dim light within 60 feet of you as if it were bright light, and in darkness as if " +
+                        "it were dim light. You can’t discern color in darkness, only shades of gray."));
+        otherBonus.add(new CharacterBonus(
+                "Hellish Resistance",
+                "You have resistance to fire damage."));
+        otherBonus.add(new CharacterBonus(
+                "Infernal Legacy",
+                "You know the thaumaturgy cantrip. When you reach 3rd level, you can cast the hellish rebuke " +
+                        "spell as a 2nd-level spell once with this trait and regain the ability to do so when you " +
+                        "finish a long rest. When you reach 5th level, you can cast the darkness spell once with this " +
+                        "trait and regain the ability to do so when you finish a long rest. Charisma is your " +
+                        "spellcasting ability for these spells."));
+        return otherBonus;
     }
 }
 
